feed6ed29820a9d63e6b6fef03af4fc996d7eee5
MaskrenhasDev <maskrenhas02@gmail.com>
2025-06-17 14:59:56 -0400

    building code in  dart language

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,30 @@
+// ignore_for_file: avoid_unnecessary_containers
+
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(
-    Container(
-      child: Text(
-        'Maskrenhas Dev Learn about flutter and development mobile',
-        textDirection: TextDirection.ltr,
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({required Key key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          'Maskrenhas Dev Mobile',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18.0,
+            backgroundColor: const Color.fromARGB(255, 255, 184, 77),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
